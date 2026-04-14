23f8c667d2ea1aba133b77a062546f374870b022
Mahendra Prajapati <sourceforget32@gmail.com>
2026-04-14 20:12:16 +0530

    mahii

--- frontend/src/utils/api.js
+++ frontend/src/utils/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: process.env.REACT_APP_API_URL || 'https://event-o-five.vercel.app/',
+  baseURL: process.env.REACT_APP_API_URL || 'http://localhost:4500/api',
   headers: {
     'Content-Type': 'application/json'
   }
